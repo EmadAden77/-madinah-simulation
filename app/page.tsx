8318123439a1ad03671293e9f52a508f44ef5979
EmadAden77 <sasfsas8@gmail.com>
2026-09-10 23:40:41 +0300

    Mount adaptive ambient soundscape controls

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,13 @@
 import dynamic from 'next/dynamic';
 
 const HistoricalMapPhoto = dynamic(() => import('@/components/HistoricalMapPhoto'), { ssr: false });
+const AmbientSoundscape = dynamic(() => import('@/components/AmbientSoundscape'), { ssr: false });
 
 export default function HomePage() {
-  return <HistoricalMapPhoto />;
+  return (
+    <>
+      <HistoricalMapPhoto />
+      <AmbientSoundscape />
+    </>
+  );
 }
