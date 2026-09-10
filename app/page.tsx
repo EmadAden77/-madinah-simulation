5fd153894752d2eb733db17eeeb2b22da565d2e2
EmadAden77 <sasfsas8@gmail.com>
2026-09-11 00:50:00 +0300

    Mount guided landmark explorer

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ import dynamic from 'next/dynamic';
 
 const HistoricalMapPhoto = dynamic(() => import('@/components/HistoricalMapPhoto'), { ssr: false });
 const AmbientSoundscape = dynamic(() => import('@/components/AmbientSoundscape'), { ssr: false });
+const LandmarkExplorerDock = dynamic(() => import('@/components/LandmarkExplorerDock'), { ssr: false });
 
 export default function HomePage() {
   return (
     <>
       <HistoricalMapPhoto />
+      <LandmarkExplorerDock />
       <AmbientSoundscape />
     </>
   );
