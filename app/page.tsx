c48bde2a5e7158c4953dd7e3c6f7d78f8f340d6b
EmadAden77 <sasfsas8@gmail.com>
2026-09-11 00:52:21 +0300

    Use immersive explorer deck for guided historical navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 'use client';
 
 import dynamic from 'next/dynamic';
+import ExplorerDeck from '@/components/ExplorerDeck';
 
 const HistoricalMapPhoto = dynamic(() => import('@/components/HistoricalMapPhoto'), { ssr: false });
 const AmbientSoundscape = dynamic(() => import('@/components/AmbientSoundscape'), { ssr: false });
-const LandmarkExplorerDock = dynamic(() => import('@/components/LandmarkExplorerDock'), { ssr: false });
 
 export default function HomePage() {
   return (
     <>
       <HistoricalMapPhoto />
-      <LandmarkExplorerDock />
+      <ExplorerDeck />
       <AmbientSoundscape />
     </>
   );
